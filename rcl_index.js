a14cc87ce6b214421a60c2da090eaa34e95a4511
ramon-cortes <110551645+ramon-cortes@users.noreply.github.com>
2022-08-28 13:34:00 -0500

    Colored menu

---
+++ rcl_index.js
@@ -0,0 +1,25 @@
+//
+const fondoCells = [];
+
+//Incrementar dependiendo del tamaño de la tabla (también agregar el id en el CSS)
+//Renglones     ↓
+let renglones = 11 * 2;
+
+for (let cellId = 0; cellId < renglones; cellId++) {
+  fondoCells.push(document.getElementById('test' + cellId));
+}
+
+function tieneFocus(x) {
+  fondoCells[x].style.background = 'rgb(0, 0, 0)';
+  fondoCells[x + 1].style.color = 'rgb(255, 255, 255)';
+}
+
+function noTieneFocus(x) {
+  fondoCells[x].style.background = 'rgb(211, 211, 211)';
+  fondoCells[x + 1].style.color = 'rgb(0, 0, 255)';
+}
+
+for (let i = 0; i < fondoCells.length; i += 2) {
+  fondoCells[i].addEventListener("mouseover", () => {tieneFocus(i);}, false);
+  fondoCells[i].addEventListener("mouseleave", () => {noTieneFocus(i);}, false);
+}
