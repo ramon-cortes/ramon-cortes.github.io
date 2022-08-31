1d537bc4b1315e09f24add69341c4e8c3f728e65
ramon-cortes <110551645+ramon-cortes@users.noreply.github.com>
2022-08-31 14:54:03 -0500

    Add files via upload

--- week05/boston_rcl.js
+++ week05/boston_rcl.js
@@ -1,6 +1,9 @@
 const bostonPeople = boston.data;
 const len = boston.data.length;
 
+//Math.abs(parseInt(document.getElementById('circlesChosen').value));
+document.getElementById('consola').innerHTML = 'Consola: ';
+
 function everyOne() {
   let htmlTabla = `<table><tr><th>#</th><th>PERSON</th><th>TITLE</th><th>SALARY</th></tr>`
   for (let i = 0; i < len; i++) {
@@ -19,13 +22,18 @@ function everyOne() {
   document.getElementById('tabla').innerHTML = htmlTabla;
 }
 
-function overOneK() {
+function overSome() {
   let htmlTabla = `<table><tr><th>#</th><th>PERSON</th><th>TITLE</th><th>SALARY</th></tr>`
+  let over = Math.abs(parseInt(document.getElementById('over').value));
+  if (over > 200000 || over < 50000) {
+    over = 1000000;
+  }
+
   let person = [];
   let position = [];
   let salary = [];
   for (let i = 0; i < len; i++) {    
-    if (bostonPeople[i][11] >= 100000) {
+    if (bostonPeople[i][11] >= over) {
       person.push(bostonPeople[i][8]);
       position.push(bostonPeople[i][9]);
       salary.push(bostonPeople[i][11]);
@@ -41,6 +49,30 @@ function overOneK() {
   }
   htmlTabla += '</table>';
   document.getElementById('tabla').innerHTML = htmlTabla;
+  document.getElementById('consola').innerHTML = 'Consola: ' + over;
+}
+
+function topSome() {
+  let htmlTabla = `<table><tr><th>#</th><th>PERSON</th><th>TITLE</th><th>SALARY</th></tr>`
+  let top = Math.abs(parseInt(document.getElementById('top').value));
+  if (top < 1000 && top > 1) {
+    let topOrganized = bostonPeople; //Unable to copy without reference to original array
+    topOrganized.sort((a, b) => b[11] - a[11]);
+    for (let i = 0; i < top; i++) {
+      htmlTabla += `<tr>
+      <td>${i + 1}</td>
+      <td>${bostonPeople[i][8]}</td>
+      <td>${bostonPeople[i][9]}</td>
+      <td>${bostonPeople[i][11]}</td>
+      </tr>`;
+    }
+
+
+  }
+  //.sort((a, b) => a - b)
+
+  document.getElementById('tabla').innerHTML = htmlTabla;
+  document.getElementById('consola').innerHTML = 'Consola: ' + top;
 }
 
 
