e27f6dfee0cb963ef08f5872b68ff7f0987bb25b
ramon-cortes <110551645+ramon-cortes@users.noreply.github.com>
2022-09-02 11:38:43 -0500

    Fading color effect

--- rcl_index.js
+++ rcl_index.js
@@ -1,22 +1,54 @@
 //
 const fondoCells = [];
+let id = [];
 
-//Incrementar dependiendo del tamaño de la tabla (también agregar el id en el CSS)
-//Renglones     ↓
-let renglones = 12 * 2;
+//Número de Renglones (celdas)
+//                ↓
+const numCeldas = 12;
+let renglones = numCeldas * 2;
 
 for (let cellId = 0; cellId < renglones; cellId++) {
   fondoCells.push(document.getElementById('test' + cellId));
 }
 
+//Convierte texto 'rgb(x, y, z)' en [x, y, z]
+function convierteRgbNumeros(rgbTexto) {
+  let rgbNumeros;
+  rgbNumeros = rgbTexto.slice(4, rgbTexto.indexOf(')'));
+  rgbNumeros = rgbNumeros.replaceAll(' ', '');
+  rgbNumeros = rgbNumeros.split(',');
+
+  for (let i = 0; i < 3; i++) {
+    rgbNumeros[i] = parseInt(rgbNumeros[i]);
+  }
+
+  return rgbNumeros;
+}
+
 function tieneFocus(x) {
+  clearTimeout(id[x]);
   fondoCells[x].style.background = 'rgb(0, 0, 0)';
-  fondoCells[x + 1].style.color = 'rgb(255, 255, 255)';
+  //fondoCells[x + 1].style.color = 'rgb(255, 255, 255)';
 }
 
 function noTieneFocus(x) {
-  fondoCells[x].style.background = 'rgb(211, 211, 211)';
-  fondoCells[x + 1].style.color = 'rgb(0, 0, 255)';
+  clearTimeout(id[x]);
+  let colorActual = convierteRgbNumeros(fondoCells[x].style.background);
+
+  for (let i = 0; i < 3; i++) {
+    if (colorActual[i] < 211) {
+      colorActual[i] += 10;
+    } else {
+      colorActual[i] = 211;
+    }
+  }
+
+  fondoCells[x].style.background = 'rgb(' + colorActual[0] + ',' + colorActual[1] + ',' + colorActual[2] + ')';
+  //fondoCells[x + 1].style.color = 'rgb(0, 0, 255)';
+
+  if (colorActual[0] < 255) {
+    id[x] = setTimeout(noTieneFocus, 100, x);
+  }
 }
 
 for (let i = 0; i < fondoCells.length; i += 2) {
