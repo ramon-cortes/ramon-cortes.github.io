c8b7accafe28715c928e924e8dfa166444eaf50f
ramon-cortes <110551645+ramon-cortes@users.noreply.github.com>
2022-09-07 09:47:47 -0500

    Add files via upload

--- rcl_index.js
+++ rcl_index.js
@@ -4,7 +4,7 @@ let id = [];
 
 //Número de Renglones (celdas)
 //                ↓
-const numCeldas = 12;
+const numCeldas = 13;
 let renglones = numCeldas * 2;
 
 for (let cellId = 0; cellId < renglones; cellId++) {
@@ -36,7 +36,7 @@ function noTieneFocus(x) {
   let colorActual = convierteRgbNumeros(fondoCells[x].style.background);
 
   for (let i = 0; i < 3; i++) {
-    if (colorActual[i] < 211) {
+    if (colorActual[i] <= 201) {
       colorActual[i] += 10;
     } else {
       colorActual[i] = 211;
@@ -46,7 +46,7 @@ function noTieneFocus(x) {
   fondoCells[x].style.background = 'rgb(' + colorActual[0] + ',' + colorActual[1] + ',' + colorActual[2] + ')';
   //fondoCells[x + 1].style.color = 'rgb(0, 0, 255)';
 
-  if (colorActual[0] < 255) {
+  if (colorActual[0] < 211) {
     id[x] = setTimeout(noTieneFocus, 100, x);
   }
 }
