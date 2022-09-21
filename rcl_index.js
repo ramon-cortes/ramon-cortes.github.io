f73f9191c6f691ab0ab277481529fd5cff033fc2
ramon-cortes <110551645+ramon-cortes@users.noreply.github.com>
2022-09-21 10:52:06 -0500

    Week 07 added

--- rcl_index.js
+++ rcl_index.js
@@ -4,7 +4,7 @@ let id = [];
 
 //Número de Renglones (celdas)
 //                ↓
-const numCeldas = 15;
+const numCeldas = 16;
 let renglones = numCeldas * 2;
 
 for (let cellId = 0; cellId < renglones; cellId++) {
